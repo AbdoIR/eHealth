69e2df0434d364d3311c42797437b3d9c2c12bfc
AbdoIR <abdoir192@gmail.com>
2026-03-14 11:33:38 +0000

    Update truffle config to change build location

--- Blockchain/truffle-config.js
+++ Blockchain/truffle-config.js
@@ -1,3 +1,4 @@
+const path = require("path");
 /**
  * Use this file to configure your truffle project. It's seeded with some
  * common settings for different networks and features like migrations,
@@ -47,6 +48,7 @@
 // const HDWalletProvider = require('@truffle/hdwallet-provider');
 
 module.exports = {
+  contracts_build_directory: path.join(__dirname, "../Frontend/src/blockchain"),
   /**
    * Networks define how you connect to your ethereum client and let you set the
    * defaults web3 uses to send transactions. If you don't specify one truffle
