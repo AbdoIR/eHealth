315dd8201d79f7998977245deaf985dc45df5093
AbdoIR <abdoir192@gmail.com>
2026-03-16 22:33:02 +0000

    Update string utility with tests.

---
+++ Frontend/src/utils/stringUtils.js
@@ -0,0 +1,9 @@
+/** Returns up-to-two uppercase initials from a full name string. */
+export function getInitials(name = '') {
+  return name
+    .split(' ')
+    .filter(Boolean)
+    .slice(0, 2)
+    .map((w) => w[0].toUpperCase())
+    .join('')
+}
